bace8540a580e7c84674e07a2d56b8d62d3d4fef
Marius Erlandsen <marius.erlandsen@nav.no>
2019-03-08 23:09:13 +0100

    adding idiomatic use of make vs new

---
+++ idiomatic/newVsMake/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	// newBlabla is for when you return a pointer
+	// makeBlabla is for when you return a type
+}
+
+type mystruct struct {
+	one string
+	two string
+}
+
+func newStruct(one, two string) *mystruct {
+	return &mystruct{
+		one: one,
+		two: two,
+	}
+}
+
+func makeStruct(one, two string) mystruct {
+	return mystruct
+}
