5b2fc1788c63a82af21aca2c850c8440eb74894a
Marius Erlandsen <marius.erlandsen@nav.no>
2019-03-08 13:10:05 +0100

    clearer comment in code

--- regexp/capgroup/main.go
+++ regexp/capgroup/main.go
@@ -9,8 +9,7 @@ func main() {
 	line := `<script>var goVersion = "go1.12";</script>`
 	
 	re := regexp.MustCompile(`(go1\.)(..)`) // two "capture groups".
-	xs := re.FindStringSubmatch(line)       // xs[0] is the whole string, []xs1 is capgroup1, xs[2] is capgroup2
-	
+	xs := re.FindStringSubmatch(line)       // xs[0] is the whole regexp match, []xs1 is capgroup1, xs[2] is capgroup2
 	ver := xs[2]
 	fmt.Println(ver)
 }
